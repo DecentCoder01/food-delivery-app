f123d0567ae4997d94c787df85dfdac1010b74d0
Rajpal <rajpalrajwara2001@gmail.com>
2025-11-24 15:13:44 +0530

    commit-2c

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,13 @@ const port = 4000;
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: [
+        "https://food-delivery-app-yfnv.vercel.app/"
+    ],
+    methods: ["GET","POST","PUT","DELETE"],
+    credentials: true
+}));
 
 // db connection
 connectDB();
