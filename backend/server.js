2c4e78623c9edf14dcf1a8ff0c8ee2598ddb7a9d
Rajpal <rajpalrajwara2001@gmail.com>
2025-11-27 02:03:43 +0530

    ..

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cors({
     origin: [
         "https://food-delivery-app-yfnv.vercel.app",
-        "https://admin-food-delivery-63oe5ls26-decentcoder01s-projects.vercel.app"
+        "https://admin-food-delivery-eight.vercel.app"
     ],
     methods: ["GET","POST","PUT","DELETE"],
     credentials: true
