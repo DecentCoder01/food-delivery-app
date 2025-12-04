9a3773175427794a7ff9b3ec347374dda34c6660
Rajpal <rajpalrajwara2001@gmail.com>
2025-12-05 02:13:45 +0530

    commit

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -30,7 +30,7 @@ const placeOrder = async (req,res)=>{
                 product_data:{
                     name:item.name
                 },
-                unit_amount:item.price
+                unit_amount:item.price*100
             },
 
             quantity:item.quantity
@@ -43,7 +43,7 @@ const placeOrder = async (req,res)=>{
                 product_data:{
                     name: "Delivery Charges"
                 },
-                unit_amount: 30
+                unit_amount: 30*100
             },
             quantity: 1
         });
